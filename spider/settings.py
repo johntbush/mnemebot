a445fbd9425a99c19da2580a62a0623a39956f80
john bush <john.bush@traxtech.com>
2018-11-19 23:51:13 -0700

    Delete settings.py

--- spider/settings.py
+++
@@ -1,5 +0,0 @@
-DOWNLOADER_MIDDLEWARES = {
-  'scrapy_splash.SplashCookiesMiddleware': 723,
-  'scrapy_splash.SplashMiddleware': 725,
-  'scrapy.downloadermiddlewares.httpcompression.HttpCompressionMiddleware': 810,
-}
